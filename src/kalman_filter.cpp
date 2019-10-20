9e370a4e8035893cb48532f798cdaf150ca57503
Hamidreza3252 <Hamid.Mirkhani@gmail.com>
2019-10-20 19:07:23 -0400

    update function-01

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -32,15 +32,36 @@ void KalmanFilter::init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 void KalmanFilter::predict()
 {
   /**
-   * TODO: predict the state
+   * predict the state
    */
+
+  // X_{kp} = F * X_{k-1} + B u_{k-1} + w_k
+  states_ = stateTransMatrix_ * states_;
+
+  // P_{kp} = F * P_{k-1} * F^{T} + Q_{k}
+  MatrixXd stateTransMatrixTranspose = stateTransMatrix_.transpose();
+  stateCovMatrix_ = stateTransMatrix_ * stateCovMatrix_ * stateTransMatrixTranspose + processCovMatrix_;
 }
 
 void KalmanFilter::update(const VectorXd &measurements)
 {
   /**
-   * TODO: update the state by using Kalman Filter equations
+   * update the state by using Kalman Filter equations
    */
+
+  // S = H * P_{k} * H^{T} + R_{km}
+  MatrixXd measurementMatrixTranspose = measurementMatrix_.transpose();
+  MatrixXd sMatrix = measurementMatrix_ * stateCovMatrix_ * measurementMatrixTranspose + measurementCovMatrix_;
+  MatrixXd sMatrixInv = sMatrix.inverse();
+
+  // Y_{k} = Z_{k_m} - H * X_{kp}
+  VectorXd yVector = measurements - measurementMatrix_ * states_;
+
+  // K = P_{kp} * H * S^{-1}
+  MatrixXd kalmanGain = stateCovMatrix_ * measurementMatrix_ * sMatrixInv;
+
+  // X_{k} = X_{kp} + K * Y_{k}
+  states_ = states_ + kalmanGain * yVector;
 }
 
 void KalmanFilter::updateEKF(const VectorXd &measurements)
